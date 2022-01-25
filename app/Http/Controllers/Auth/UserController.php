293274d7b4f3523c27f5e00ce2d6bccefb1bb333
Bidhan <bidhanbaniya789@gmail.com>
2022-01-25 14:01:42 +0545

    user change password used QB ORM doesnt work here

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -64,11 +64,14 @@ class UserController extends Controller
     public function passwordChange(Request $request, User $user): RedirectResponse
     {
         $request->validate(['password' => ['required', 'string', 'min:8', 'confirmed']]);
-        if (Hash::check($request->password, $user->password)) {
-            Alert::error('पासवोर्ड पहिलानै प्रयोग गरिएको छ ');
-            return redirect()->back();
-        }
-        $user->update(['password' => Hash::make($request->password)]);
+        // if (Hash::check($request->password, $user->password)) {
+        //     Alert::error('पासवोर्ड पहिलानै प्रयोग गरिएको छ ');
+        //     return redirect()->back();
+        // }
+        // using query builder because orm is not working here :(
+        DB::table('users')->where('id',$user->id)->update([
+            'password' => Hash::make($request->password)
+        ]);
         toast('प्रयोगकर्ताको पासवोर्ड सच्याउन सफल भयो', 'success');
         return redirect()->route('user.index');
     }
