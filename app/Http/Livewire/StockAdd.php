cf6d10c1b0d2b0cc02485f0b3b9a1bb0cd42cde3
Bidhan <bidhanbaniya789@gmail.com>
2022-02-09 16:58:17 +0545

    Played with livewire for stock

--- app/Http/Livewire/StockAdd.php
+++ app/Http/Livewire/StockAdd.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\fertilizer\fertilizer;
 use App\Models\setting\crop;
 use Livewire\Component;
 
@@ -9,13 +10,39 @@ class StockAdd extends Component
 {
 
     public $crop_types;
-    public $crop_type = '';
+    public $crop_type_id = '';
+    public $stock_type = '';
+    public $crop_id = '';
+    public $fertilizer_id = '';
+    public $is_crop = TRUE;
+    public $is_fertilizer = FALSE;
     public $crops = [];
+    public $fertilizers = [];
+
+    public function mount()
+    {
+        $this->fertilizers = fertilizer::query()->get();
+    }
 
     public function render()
     {
-        if ($this->crop_type != '') {
-            $this->crops = crop::query()->get();        
+        if ($this->stock_type != '') {
+            if (in_array($this->stock_type, ['seed', 'fertilizer'])) {  // setting double security 
+                if ($this->stock_type == 'fertilizer') {
+                    $this->fertilizers = fertilizer::query()->get();
+                    $this->is_crop = false;
+                    $this->is_fertilizer = TRUE;
+                }
+                if ($this->stock_type == 'seed') {
+                    if ($this->crop_type_id != '') {
+                        $this->crops = crop::query()
+                            ->where('crop_type_id', $this->crop_type_id)
+                            ->get();
+                    }
+                    $this->is_crop = true;
+                    $this->is_fertilizer = false;
+                }
+            }
         }
         return view('livewire.stock-add');
     }
--- resources/views/livewire/stock-add.blade.php
+++ resources/views/livewire/stock-add.blade.php
@@ -3,14 +3,13 @@
         <div class="input-group input-group-sm">
             <div class="input-group-prepend">
                 <span class="input-group-text">
-                    {{ __('बालीको प्रकार') }} <span class="text-danger px-1 font-weight-bold">*</span>
+                    {{ __('Stock type') }} <span class="text-danger px-1 font-weight-bold">*</span>
                 </span>
             </div>
-            <select wire:model="crop_type_id" class="form-control form-control-sm">
+            <select wire:model="stock_type" class="form-control form-control-sm">
                 <option value="">{{ __('---छान्नुहोस्----') }}</option>
-                @foreach ($crop_types as $crop_type)
-                    <option value="{{ $crop_type->id }}">{{ $crop_type->name }}</option>
-                @endforeach
+                <option value="fertilizer">{{ __('मल') }}</option>
+                <option value="seed">{{ __('बिरुवा') }}</option>
             </select>
             @error('name')
                 <p class="invalid-feedback mb-0" style="font-size: 0.9rem">
@@ -19,24 +18,69 @@
             @enderror
         </div>
     </div>
+    @if ($is_crop)
+        <div class="col-6">
+            <div class="input-group input-group-sm">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        {{ __('बालीको प्रकार') }} <span class="text-danger px-1 font-weight-bold">*</span>
+                    </span>
+                </div>
+                <select wire:model="crop_type_id" class="form-control form-control-sm">
+                    <option value="" selected>{{ __('---छान्नुहोस्----') }}</option>
+                    @foreach ($crop_types as $crop_type)
+                        <option value="{{ $crop_type->id }}">{{ $crop_type->name }}</option>
+                    @endforeach
+                </select>
+                @error('name')
+                    <p class="invalid-feedback mb-0" style="font-size: 0.9rem">
+                        {{ __('STOCKको फिल्ड खाली छ ') }}
+                    </p>
+                @enderror
+            </div>
+        </div>
+        <div class="col-6 my-2">
+            <div class="input-group input-group-sm">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        {{ __('बालि') }} <span class="text-danger px-1 font-weight-bold">*</span>
+                    </span>
+                </div>
+                <select wire:model="crop_id" class="form-control form-control-sm">
+                    <option value="">{{ __('---छान्नुहोस्----') }}</option>
+                    @foreach ($crops as $crop)
+                        <option value="{{ $crop->id }}">{{ $crop->name }}</option>
+                    @endforeach
+                </select>
+                @error('name')
+                    <p class="invalid-feedback mb-0" style="font-size: 0.9rem">
+                        {{ __('STOCKको फिल्ड खाली छ ') }}
+                    </p>
+                @enderror
+            </div>
+            <input type="hidden" name="crop_id" value="{{$crop_id ?? ''}}">
+        </div>
+    @endif
+    @if ($is_fertilizer)
     <div class="col-6">
         <div class="input-group input-group-sm">
             <div class="input-group-prepend">
                 <span class="input-group-text">
-                    {{ __('STOCK') }} <span class="text-danger px-1 font-weight-bold">*</span>
+                    {{ __('मल') }} <span class="text-danger px-1 font-weight-bold">*</span>
                 </span>
             </div>
-            <select wire:model="crop_type_id" class="form-control form-control-sm">
+            <select wire:model="fertilizer_id" class="form-control form-control-sm">
                 <option value="">{{ __('---छान्नुहोस्----') }}</option>
-                @foreach ($crop_types as $crop_type)
-                    <option value="{{ $crop_type->id }}">{{ $crop_type->name }}</option>
+                @foreach ($fertilizers as $fertilizer)
+                    <option value="{{ $fertilizer->id }}">{{ $fertilizer->name }}</option>
                 @endforeach
             </select>
             @error('name')
                 <p class="invalid-feedback mb-0" style="font-size: 0.9rem">
-                    {{ __('STOCKको फिल्ड खाली छ ') }}
+                    {{ __('मलको फिल्ड खाली छ ') }}
                 </p>
             @enderror
         </div>
     </div>
+    @endif
 </div>
